e33bdda4e4b0e6de7cb204d11aa8bfbcfad6095f
Remy Meier <remad@web.de>
2024-09-16 20:41:18 +0200

    master: index shows menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
