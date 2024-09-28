e3da624bab7ba6f0b5c8300fe10b7b9c83901986
Remy Meier <remad@web.de>
2024-09-28 18:25:05 +0200

    master: routing done by click events

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+
+  constructor(private router : Router) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  navigateToRoomsAdmin() {
+    this.router.navigate(['admin','rooms']);
+  }
+
+  navigateToUsersAdmin() {
+    this.router.navigate(['admin','users']);
+  }
+
+  navigateToCalendar() {
+    this.router.navigate(['']);
+  }
+
+  navigateToHome() {
+    this.navigateToCalendar();
+  }
 
 }
