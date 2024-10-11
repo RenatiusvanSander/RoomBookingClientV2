22c2cdaddbfad6b8be7640223d31e3681d11e956
Remy Meier <remad@web.de>
2024-10-11 23:12:30 +0200

    master: validation on forms

--- src/app/admin/users/user-edit/user-edit.component.ts
+++ src/app/admin/users/user-edit/user-edit.component.ts
@@ -21,6 +21,10 @@ export class UserEditComponent implements OnInit {
 
   password2: string;
 
+  nameIsValid = false;
+  passwordsAreValid = false;
+  passwordsMatch = false;
+
   constructor(private dataService: DataService, private router: Router) {
     this.user = new User();
     this.message = '';
@@ -31,6 +35,8 @@ export class UserEditComponent implements OnInit {
 
   ngOnInit(): void {
     this.formUser = Object.assign({}, this.user);
+    this.checkIfNameIsValid();
+    this.checkIfPasswordsAreValid();
   }
 
   onSubmit() {
@@ -47,6 +53,29 @@ export class UserEditComponent implements OnInit {
           this.router.navigate(['admin','users'], {queryParams: {action: 'view', id : user.id}});
         }
       );
+    }
+  }
+
+  checkIfNameIsValid() {
+    if (this.formUser.name) {
+      this.nameIsValid = this.formUser.name.trim().length > 0;
+    } else {
+      this.nameIsValid = false;
+    }
+  }
+
+  checkIfPasswordsAreValid() {
+    if (this.formUser.id != null) {
+      this.passwordsAreValid = true;
+      this.passwordsMatch = true;
+    } else {
+      this.passwordsMatch = this.password === this.password2;
+      
+      if (this.password) {
+        this.passwordsAreValid =this.password.trim().length > 0;
+      } else {
+        this.passwordsAreValid = false;
+      }
     }
   }
 }
