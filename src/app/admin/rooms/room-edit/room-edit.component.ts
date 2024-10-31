1ddddb31e4aa710fd65711948b6345dd09039354
Remy Meier <remad@web.de>
2024-10-31 22:57:41 +0100

    master: type problems solved

--- src/app/admin/rooms/room-edit/room-edit.component.ts
+++ src/app/admin/rooms/room-edit/room-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Room } from '../../../model/room';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Layout, LayoutCapacity, Room } from '../../../model/room';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-room-edit',
@@ -12,10 +12,14 @@ export class RoomEditComponent implements OnInit{
   @Input()
   room: Room;
 
-  roomForm = new FormGroup(
+  layouts = Object.keys(Layout);
+  layoutEnum = Layout;
+  layoutTypes: Array<string> = Object.keys(Layout).filter(key => isNaN(+key));
+
+  roomForm = new FormGroup<{[key: string]:FormControl|FormArray}>(
     {
       roomName : new FormControl('roomName'),
-      location: new FormControl('location')
+      location: new FormControl('location'),
     }
   );
   
@@ -28,12 +32,28 @@ export class RoomEditComponent implements OnInit{
       roomName:  this.room.name,
       location: this.room.location
     });
+
+    for (const layout of this.layouts) {
+      this.roomForm.addControl(`layout${layout}`, new FormControl(`layout${layout}`));
+    }
   }
 
   onSubmit() {
     this.room.name = this.roomForm.controls['roomName'].value ?? '';
     this.room.location = this.roomForm.value['location'] ?? '';
+    this.room.capacities = new Array<LayoutCapacity>();
+    
+    for(const layout of this.layouts) {
+      const layoutCapacity = new LayoutCapacity();
+      layoutCapacity.layout = this.getLayoutByKey(layout);
+      layoutCapacity.capacity = this.roomForm.controls[`layout${layout}`].value;
+      this.room.capacities.push(layoutCapacity);
+    }
     console.log(this.room);
+  }
+
+  getLayoutByKey(layoutKey: string): Layout {
+    return this.layoutEnum[layoutKey as keyof typeof Layout];
   }
 
 }
