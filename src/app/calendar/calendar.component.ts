7271e5a1d6b7ba70dde4f32305444548a7c53f69
Remy Meier <remad@web.de>
2024-11-04 19:38:25 +0100

    master: formatDate applied

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
+
+  constructor() {}
+
+  selectedDate = new Date();
+
+  ngOnInit(): void {
+    const date : string = formatDate(this.selectedDate, 'yyyy-MM-dd', 'en-UK');
+  }
 
 }
